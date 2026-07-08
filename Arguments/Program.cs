182e0d41ca1df6db297fc07f0ce7de961468e4a8
kecin2019 <1139962358@qq.com>
2026-07-08 17:48:41 +0800

    #4 args-help

--- Arguments/Program.cs
+++ Arguments/Program.cs
@@ -2,6 +2,13 @@
 
 WriteLine($"There are {args.Length} arguments.");
 
+if (args.Length == 1 && args[0] is "-h" or "--help")
+{
+    WriteLine("Usage: dotnet run <foreground> <background> <cursor-size>");
+    WriteLine("Example: dotnet run red yellow 50");
+    return;
+}
+
 foreach (string arg in args)
 {
     WriteLine(arg);
@@ -11,6 +18,7 @@ if (args.Length < 3)
 {
     WriteLine("You must specify two colors and cursor size, e.g.");
     WriteLine("dotnet run red yellow 50");
+    WriteLine("Run with --help to show usage.");
     return;
 }
 
