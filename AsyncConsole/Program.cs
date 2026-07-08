25b836577665ab9d9384c193e42148a2e873768a
kecin2019 <1139962358@qq.com>
2026-07-08 16:52:42 +0800

    #3 AsyncConsole

---
+++ AsyncConsole/Program.cs
@@ -0,0 +1,5 @@
+﻿HttpClient client = new();
+
+HttpResponseMessage response = await client.GetAsync("http://www.baidu.com");
+
+WriteLine("Baidu's home page has {0:N0} bytes.", response.Content.Headers.ContentLength);
