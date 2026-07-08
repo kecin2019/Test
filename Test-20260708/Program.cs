93eb22de2777277b1038f0c74b55aa836dbbde25
kecin2019 <1139962358@qq.com>
2026-07-08 16:22:23 +0800

    #1 AboutMyEniveronment

--- Test-20260708/Program.cs
+++ Test-20260708/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace AboutMyEniveronment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Environment.CurrentDirectory);
+            Console.WriteLine(Environment.OSVersion.VersionString);
+            Console.WriteLine("Namespace:{0}", typeof(Program).Namespace);
+        }
+    }
+}
+
